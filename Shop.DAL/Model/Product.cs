c6162b121ce0fab950dfdb41566d3582d0703630
васильева <@223_16>
2018-04-26 20:22:34 +0600

    Shop

---
+++ Shop.DAL/Model/Product.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop.DAL.Model
+{
+    class Product
+    {
+        public class Currency
+        {
+            public int CurCode { get; set; }
+            public string CurName { get; set; }
+
+            public override string ToString()
+            {
+
+
+                string str = string.Format("{0} ({1})", CurName, CurCode);
+                return str;
+                   
+            }
+        }
+        public string Name { get; set; }
+
+        private double Price_;
+        public double Price
+        {
+            get { return Price_; }
+            set
+            {
+                if (value < 0)
+                {
+                    Price_ = 0;
+                }
+                else Price_ = value;
+            }
+        }
+
+        public Currency Cur { get; set; }
+
+        public int Barcode { get; set; }
+
+        public int Quantity { get; set; }
+
+        public double ExpiredDay { get; set; }
+
+        public DateTime DateOfProduction { get; set; }
+
+        private DateTime ExpiredTime_;
+
+        public DateTime ExpiredTime
+        {
+            get
+            {
+                return ExpiredTime_;
+            }
+
+
+
+            set
+            {
+                double TotalDays = (value - DateOfProduction).TotalDays;
+
+
+                if (TotalDays < 0)
+                {
+                    ExpiredTime_ = DateOfProduction.AddDays(ExpiredDay);
+                }
+                else if (TotalDays > ExpiredDay)
+                {
+                    ExpiredTime_ = DateOfProduction.AddDays(ExpiredDay);
+                }
+                else ExpiredTime_ = value;
+
+            }
+        }
+
+        public void PrintInfo()
+        {
+            Console.WriteLine("{0} - {1} - ({2} - {3}) - {4} {5}", Barcode, Name, DateOfProduction, ExpiredTime, Price, Cur);
+        }
+    }
+}
+
